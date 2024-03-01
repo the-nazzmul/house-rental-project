69be4bafe6319c9385c0d5fc716077fd9f01e6db
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2024-03-01 15:58:10 +0600

    initial setup

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>I am a footer</div>;
+};
+
+export default Footer;
---
+++ components/navbar/MenuItem.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface IMenuItemsProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem: React.FC<IMenuItemsProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItem;
